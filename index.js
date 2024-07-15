965b7eeee20121cfd05732bad50372fd01bb284b
Aleksey <gamer5543@mail.ru>
2024-07-15 23:21:38 +0400

    add router in index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
+const userRouter = require('./routers/user.js')
+
 const express = require('express')
-const pool = require('./db')
 
 const PORT = 8080
 
 const app = express()
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-    try {
-        const data = await pool.query(`SELECT * FROM public.user`)
-        res.status(200).send(data.rows)
-    } catch (error) {
-        console.log(error)
-        res.sendStatus(500)
-    }
-})
+app.use('/api/users',userRouter)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
