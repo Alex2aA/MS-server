ef858ff2ad6ced6fe65bcf7bd75658e3da45e2f6
Aleksey <gamer5543@mail.ru>
2024-07-15 23:21:00 +0400

    refactor config.js file add some keys

---
+++ config.js
@@ -0,0 +1,10 @@
+//jwt key sign
+module.exports = {
+    url_api: 'http://localhost:8080',
+    jwt_access_secret: 'jwt-secret-key',
+    jwt_refresh_secret: 'jwt-refresh-secret-key',
+    smtp_host: 'smtp.gmail.com',
+    smtp_port: 465,
+    smtp_user: 'msapplication.business@gmail.com',
+    smtp_password: 'jsma ffod ehhf skkx'
+}
