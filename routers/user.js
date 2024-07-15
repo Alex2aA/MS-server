dc2baa5a99855124c3ac2990e22aa7353f8cd5be
Aleksey <gamer5543@mail.ru>
2024-07-15 23:23:00 +0400

    create user router

---
+++ routers/user.js
@@ -0,0 +1,27 @@
+//init router-express
+const Router = require('express')
+
+//export express-validator
+const {check} = require("express-validator")
+
+//export middlewares functions
+const authMiddleware = require('../middlewares/authMiddleware')
+
+//export routing classes
+const usersController = require('../controllers/usersController')
+
+//create new class userRouter
+const userRouter = new Router()
+
+userRouter.get('/all_users',authMiddleware, usersController.getUsers)
+userRouter.post('/registration', [
+    check('username', "The login field cannot be empty").notEmpty(),
+    check('password', "The password field must be longer than 8 characters").isLength({min:8}),
+    check('password', 'The password field must be less than 15 characters').isLength({max:15}),
+    check('email', 'Email must be correct').isEmail()
+], usersController.registartion )
+userRouter.post('/login', usersController.login )
+userRouter.get('/refresh', usersController.refresh )
+userRouter.get('/activate/:link', usersController.activate)
+
+module.exports = userRouter
