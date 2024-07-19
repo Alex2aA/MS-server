5b462f3329f706878016f2812ec073b8efddaff5
Aleksey <gamer5543@mail.ru>
2024-07-19 23:27:13 +0400

    create another api routers

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const userRouter = require('./routers/user.js')
+const fileRouter = require('./routers/file.js')
+const messageRouter = require('./routers/message.js')
+const fileUploader = require('express-fileupload')
+const fs = require('fs')
 
 const express = require('express')
 
@@ -6,7 +10,14 @@ const PORT = 8080
 
 const app = express()
 app.use(express.json())
+app.use(fileUploader())
 
 app.use('/api/users',userRouter)
+app.use('/api/upload', fileRouter)
+app.use('/api/message', messageRouter)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+if(!fs.existsSync(__dirname + '/users_data')) {
+    fs.mkdirSync(__dirname + '/users_data')
+}
