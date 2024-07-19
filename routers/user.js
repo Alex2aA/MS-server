ae810de20b7f5acd6df5e4cb4e988e63a92650d9
Aleksey <gamer5543@mail.ru>
2024-07-19 23:28:55 +0400

    bug fixed on user router file

--- routers/user.js
+++ routers/user.js
@@ -16,12 +16,13 @@ const userRouter = new Router()
 userRouter.get('/all_users',authMiddleware, usersController.getUsers)
 userRouter.post('/registration', [
     check('username', "The login field cannot be empty").notEmpty(),
+    check('username', "The username field must be less than 15 characters").isLength({max:20}),
     check('password', "The password field must be longer than 8 characters").isLength({min:8}),
     check('password', 'The password field must be less than 15 characters').isLength({max:15}),
     check('email', 'Email must be correct').isEmail()
 ], usersController.registartion )
 userRouter.post('/login', usersController.login )
 userRouter.get('/refresh', usersController.refresh )
-userRouter.get('/activate/:link', usersController.activate)
+userRouter.get('/activate/:link',authMiddleware, usersController.activate)
 
 module.exports = userRouter
